3484d15162cfdaf1f0be03638a475f2479d1bd25
MixPiyadanai <mix.piyadanai@gmail.com>
2023-03-06 21:39:53 +0700

    6/3/2023

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_auth/Screens/Welcome/welcome_screen.dart';
+import 'package:flutter_auth/Screens/Login/login_screen.dart';
 import 'package:flutter_auth/constants.dart';
 
 void main() => runApp(const MyApp());
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
               borderSide: BorderSide.none,
             ),
           )),
-      home: const WelcomeScreen(),
+      home: const LoginScreen(),
     );
   }
 }
